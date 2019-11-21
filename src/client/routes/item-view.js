9225bcb6bb043a9481d6da3f53ab41cc66d7feae
nbiton <naor.biton@gmail.com>
2019-11-21 11:05:34 +0800

    new fixed outfit display proportions

--- src/client/routes/item-view.js
+++ src/client/routes/item-view.js
@@ -8,19 +8,19 @@ import { CardsWrapper } from '../components/cards-wrapper'
 const outfitParts = [
   {
     name: 'bottoms',
-    classes: 'bottom-0 left-1 w-60'
+    classes: 'bottom-0 left-1 w3 w4-ns'
   },
   {
     name: 'shirts',
-    classes: 'top-0 left-0 w-50'
+    classes: 'top-0 left-0 w3 w4-ns'
   },
   {
     name: 'jackets',
-    classes: 'top-0 right-0 w-60'
+    classes: 'top-0 right-0 w3 w4-ns'
   },
   {
     name: 'shoes',
-    classes: 'bottom-0 right-1-ns right-0 w-third'
+    classes: 'bottom-0 right-1-ns right-0 w2 w3-ns'
   }
 ]
 
@@ -50,7 +50,7 @@ export const ItemView = props => {
         <CardsWrapper>
           {outfits.map(outfit => (
             <div className='pa2 mb4 w-50 relative flex flex-column items-center' key={outfit._id}>
-              <div className='w-60-ns w-80 h5_5 relative'>
+              <div className='w4 w5-ns h4 h5-ns relative'>
                 {outfitParts.map(part => (
                   <OutfitPart key={part.name} outfit={outfit} partName={part.name} classes={part.classes} />
                 ))}
