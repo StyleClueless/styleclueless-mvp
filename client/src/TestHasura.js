6c340f06e8d225b5aac542f8b5bf9b9859a87b10
or <zobis2@gmail.com>
2020-03-28 21:48:49 +0300

    trying to play

--- client/src/TestHasura.js
+++ client/src/TestHasura.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { withApollo,ApolloConsumer } from 'react-apollo';
+import React, {Component} from 'react';
+import {withApollo, ApolloConsumer} from 'react-apollo';
 
 import gql from 'graphql-tag';
 
 class TestHasura extends Component {
-    state = { dataProvider: null };
+    state = {dataProvider: null};
 
     async componentWillMount() {
         console.log('x');
         debugger;
-         const TEST_QUERY = gql`
+        const TEST_QUERY = gql`
    query MyQuery {
   clients {
     created_at
@@ -23,12 +23,18 @@ class TestHasura extends Component {
   }
 }
 `;
-        const { data } = await this.props.client.query({
-            query: TEST_QUERY,
-            variables: { count_id: this.state.count_id },
-            fetchPolicy: 'network-only',
-        });
-        console.log(data);
+        try {
+            const {data} = await this.props.client.query({
+                query: TEST_QUERY,
+                variables: {count_id: this.state.count_id},
+                fetchPolicy: 'network-only',
+            });
+            console.log(data);
+        }
+        catch (e) {
+            console.error(e);
+        }
+
     }
 
     componentWillUnmount() {
