dc5b844ced39d6316ca2020a0a3018ef18b4e59e
or <zobis2@gmail.com>
2020-04-10 19:51:54 +0300

    STYLING

--- client/src/SelectHighlighted.js
+++ client/src/SelectHighlighted.js
@@ -1,3 +1,7 @@
+
+import React from 'react'
+import styles from './red.css'; // Import css modules stylesheet as styles
+
 const css=
 
 
@@ -32,15 +36,17 @@ class SelectHighlighted extends React.Component {
     }
 
     render () {
-        const {options_array,color}=this.props?this.props:{};
+        // let {options_array,color}=this.props?this.props:{};
+        // color='red';
         return (
-            <select className="select" size={5} value={this.state.option} onChange={this.handleOptionsChange}>
-                <option className="opt" value='1'>Option 1</option>
-                <option className="opt" value='2'>Option 2</option>
-                <option className="opt" value='3'>Option 3</option>
-                <option className="opt" value='4'>Option 4</option>
-                <option className="opt" value='5'>Option 5</option>
+            <select className={styles.select} size={5} value={this.state.option} onChange={this.handleOptionsChange}>
+                <option className={styles.opt} value='1'>Option 1</option>
+                <option className={styles.opt} value='2'>Option 2</option>
+                <option className={styles.opt} value='3'>Option 3</option>
+                <option className={styles.opt} value='4'>Option 4</option>
+                <option className={styles.opt} value='5'>Option 5</option>
             </select>
         );
     }
 }
+export default SelectHighlighted;
--- client/src/app.js
+++ client/src/app.js
@@ -1,75 +1,79 @@
 import React from 'react';
 // import { Route } from 'react-router';
-import { ApolloConsumer } from 'react-apollo';
-import { ApolloProvider } from 'react-apollo';
-import { apollo_client } from './apolloClient';
-import { Home } from './routes/home.js'
+import {ApolloConsumer} from 'react-apollo';
+import {ApolloProvider} from 'react-apollo';
+import {apollo_client} from './apolloClient';
+import {Home} from './routes/home.js'
 import {
     Switch,
     Route,
     Link, BrowserRouter
 } from 'react-router-dom'
-import { categories } from './categories-config'
-import { ItemsList } from './routes/items-list'
-import { ItemView } from './routes/item-view'
-import { withApollo } from 'react-apollo';
+import {categories} from './categories-config'
+import {ItemsList} from './routes/items-list'
+import {ItemView} from './routes/item-view'
+import {withApollo} from 'react-apollo';
 import TestHasura from "./TestHasura";
+import SelectHighlighted from "./SelectHighlighted";
 
 // import Article from '../containers/Article';
 
 export const App = () => {
-  let scrollEl
-  let lastRouteName
-  const scrollToTop = () => {
-    if (scrollEl) scrollEl.scrollTop = 0
-    if (typeof window !== 'undefined') window.scrollTop = 0
-  }
-  return (
-      <ApolloProvider client={apollo_client}>
-          <TestHasura/>
-
-    <div style={{display:'none'}} className='pv2 h-100 flex flex-column'>
-        <div className='flex justify-center baskerville'>
-        <div className='dib relative bt bb b--dark-gray w-100 w-60-l'>
-          {['Home'].concat(categories).map(catName => (
-            <Route key={catName} path='/:routeName' children={({ match }) => {
-              const isMatching = !match && catName === 'Home' || (match && catName.toLowerCase() === match.params.routeName)
-              if (match && match.params.routeName !== lastRouteName) {
-                lastRouteName = match.params.routerName
-                scrollToTop()
-              }
-              return (
-                <div className={'dib tc pv1 w-20 relative' + (isMatching ? ' bg-dark-gray' : '')}>
-                  <Link
-                    className={'link dib pv2 w-100 f6 f5-ns ' + (isMatching ? 'white' : 'dark-gray')}
-                    to={catName === 'Home' ? '/' : `/${catName.toLowerCase()}`}
-                  >
-                    {catName}
-                  </Link>
+    let scrollEl
+    let lastRouteName
+    const scrollToTop = () => {
+        if (scrollEl) scrollEl.scrollTop = 0
+        if (typeof window !== 'undefined') window.scrollTop = 0
+    }
+    return (
+        <ApolloProvider client={apollo_client}>
+            <TestHasura/>
+            <SelectHighlighted
+           />
+            <div style={{display: 'none'}} className='pv2 h-100 flex flex-column'>
+                <div className='flex justify-center baskerville'>
+                    <div className='dib relative bt bb b--dark-gray w-100 w-60-l'>
+                        {['Home'].concat(categories).map(catName => (
+                            <Route key={catName} path='/:routeName' children={({match}) => {
+                                const isMatching = !match && catName === 'Home' || (match && catName.toLowerCase() === match.params.routeName)
+                                if (match && match.params.routeName !== lastRouteName) {
+                                    lastRouteName = match.params.routerName
+                                    scrollToTop()
+                                }
+                                return (
+                                    <div className={'dib tc pv1 w-20 relative' + (isMatching ? ' bg-dark-gray' : '')}>
+                                        <Link
+                                            className={'link dib pv2 w-100 f6 f5-ns ' + (isMatching ? 'white' : 'dark-gray')}
+                                            to={catName === 'Home' ? '/' : `/${catName.toLowerCase()}`}
+                                        >
+                                            {catName}
+                                        </Link>
+                                    </div>
+                                )
+                            }}/>
+                        ))}
+                    </div>
                 </div>
-              )
-            }} />
-          ))}
-        </div>
-      </div>
-      <div className='flex-auto overflow-auto pv2' ref={el => { scrollEl = el }}>
-        <Switch>
-          <Route path="/:itemsType/:itemCode" component={ItemView} exact />
-          <Route path="/:itemsType" component={ItemsList} exact />
-          <Route path="/" component={Home} exact />
-        </Switch>
-      </div>
-      {/*<ItemsList />*/}
-      <div className='flex justify-center'>
-        <div className='roboto tc dark-gray pv2 bw1 bt bb b--dark-gray w-100 w-60-l'>
-          StyleClueless™
-        </div>
-      </div>
+                <div className='flex-auto overflow-auto pv2' ref={el => {
+                    scrollEl = el
+                }}>
+                    <Switch>
+                        <Route path="/:itemsType/:itemCode" component={ItemView} exact/>
+                        <Route path="/:itemsType" component={ItemsList} exact/>
+                        <Route path="/" component={Home} exact/>
+                    </Switch>
+                </div>
+                {/*<ItemsList />*/}
+                <div className='flex justify-center'>
+                    <div className='roboto tc dark-gray pv2 bw1 bt bb b--dark-gray w-100 w-60-l'>
+                        StyleClueless™
+                    </div>
+                </div>
 
 
-    </div>
-      </ApolloProvider>
+            </div>
+        </ApolloProvider>
 
 
-      )
+    )
 }
